f5b33ac2f9006893c743a6647cef21e63d73e796
Have Samuel <samiehave9@gmail.com>
2023-06-17 03:26:43 +0300

    Making the search input field interactive

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+/* eslint-disable no-return-assign */
+// Getting all required Elements
+const search = document.querySelector('.search-input');
+const inputField = search.querySelector('.input-data');
+const showRequest = document.querySelector('.autocom-box');
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const fruits = [
+  'Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry',
+  'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry',
+  'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson',
+  'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig',
+  'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry',
+  'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry',
+  'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee',
+  'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew',
+  'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance',
+  'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine',
+  'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon',
+  'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo',
+  'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant',
+  'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry',
+  'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
-  const results = [];
+// If User presses any button and Releases
+inputField.onkeyup = (eve) => {
+  const userData = eve.target.value; // User Entered Data
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = fruits.filter((data) => data.toLocaleLowerCase());
+    // Shows all the fruits
+    console.log(emptyArray);
+  }
+  emptyArray = emptyArray.map((data) => data = `<li>${data}</li>`);
+  console.log(emptyArray);
+};
 
-  return results;
-}
+// function search(str) {
+//   const results = [];
 
-function searchHandler(e) {
+//   return results;
+// }
 
-}
+// function searchHandler(e) {
 
-function showSuggestions(results, inputVal) {
+// }
 
-}
+// function showSuggestions(results, inputVal) {
 
-function useSuggestion(e) {
- 
-}
+// }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+// function useSuggestion(e) {
+
+// }
+
+// input.addEventListener('keyup', searchHandler);
+// suggestions.addEventListener('click', useSuggestion);
