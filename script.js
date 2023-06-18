970ca76cabfb331f7af9bcfdad5c49e7263bf4c6
Have Samuel <samiehave9@gmail.com>
2023-06-18 03:36:39 +0300

    Remove code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-/* eslint-disable no-use-before-define */
-/* eslint-disable prefer-template */
-/* eslint-disable arrow-body-style */
-/* eslint-disable max-len */
-/* eslint-disable no-return-assign */
 // Getting all required Elements
 const search = document.querySelector('.search-input');
 const inputField = search.querySelector('.input-data');
@@ -27,51 +20,3 @@ const fruits = [
   'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry',
   'Tamarillo', 'Tamarind', 'Yuzu', 'Tomatoe 🍎'];
 
-function displayOptions(list) {
-  let listData;
-  if (!list.lentgth) {
-    userValue = inputField.value;
-    listData = '<li>' + userValue + '</li>';
-  } else {
-    listData = list.join('');
-  }
-  showRequest.innerHTML = listData;
-}
-
-// If User presses any button and Releases
-inputField.onkeyup = (eve) => {
-  const userData = eve.target.value; // User Entered Data
-  let emptyArray = [];
-  if (userData) {
-    emptyArray = fruits.filter((data) => {
-    //  Filters the array value and user character
-    // to lowercase and returns only those words/sentences which start with the users
-    // Entered Word/sentence
-      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-    });
-    // Shows all the fruits
-
-    // Putting all outputs in the LIs
-    // Also the {startsWith} function
-    emptyArray = emptyArray.map((data) => {
-      return data = '<li>' + data + '</li>';
-    });
-    // console.log(emptyArray);
-    search.classList.add('active'); // Show autocomplete box
-    displayOptions(emptyArray);
-    const allList = showRequest.querySelectorAll('li');
-    for (let i = 0; i < allList.length; i += 1) {
-      // Adding onclick attribute in all li tag
-      allList[i].setAttribute('onclick', 'select(this)');
-    }
-  } else {
-    search.classList.remove('active'); // Hide autocomplete box
-  }
-};
-
-function select(element) {
-  const selectUserData = element.textContent;
-  console.log(selectUserData);
-  inputField.value = selectUserData; // Passing the user selected list item data in textfield
-  search.classList.remove('active'); // Hide autocomplete box
-}
